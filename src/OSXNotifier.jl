28f5e53c9d202bd6b1f637a73f877c771447191e
Jonas Rauber <git@jonasrauber.de>
2015-05-03 13:08:15 +0200

    support for more parameters and removing notifications

--- src/OSXNotifier.jl
+++ src/OSXNotifier.jl
@@ -13,8 +13,30 @@ else
     error("OSXNotifier not properly installed. Please run Pkg.build(\"OSXNotifier\")")
 end
 
-function notify(message)
-    run(`$terminalnotifier -message $message`)
+function notify(message=""; title="Julia", subtitle="", open="", group="", sound=false, sender="org.julialang")
+	if group != "" && sound != "" && sound != false
+		run(`$terminalnotifier -sender $sender -message $message -title $title -subtitle $subtitle -open $open -group $group -sound $sound`)
+	elseif sound != "" && sound != false
+		run(`$terminalnotifier -sender $sender -message $message -title $title -subtitle $subtitle -open $open -sound $sound`)
+	elseif group != ""
+		run(`$terminalnotifier -sender $sender -message $message -title $title -subtitle $subtitle -open $open -group $group`)
+	else
+		run(`$terminalnotifier -sender $sender -message $message -title $title -subtitle $subtitle -open $open`)
+	end
 end
+
+function remove(group="ALL"; sender="org.julialang")
+	run(`$terminalnotifier -remove $group -sender $sender`)
+end
+
+#function list(group="ALL")
+#	lines = readall(`$terminalnotifier -list $group`)
+#	lines = split(lines, "\n")
+#	lines = map(line -> split(line, "\t"), lines)
+#	lines = map(line -> map(element -> convert(ASCIIString, element), line), lines)
+#	keys = lines[1]
+#	lines = lines[2:end-1]
+#	map(line -> Dict(zip(keys, line)), lines)
+#end
 
 end # module
