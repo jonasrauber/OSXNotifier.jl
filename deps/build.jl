7b8d80ae58f84ec90ff06a01d32f890de03d7915
Jonas Rauber <git@jonasrauber.de>
2015-05-03 10:40:00 +0200

    trying to get BinDeps working

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,13 @@
 using BinDeps
+using Compat
+
 @BinDeps.setup
+
 terminalnotifier = library_dependency("terminal-notifier", os = :Darwin)
 
-using Homebrew
-provides(Homebrew.HB, "terminal-notifier", terminalnotifier, os = :Darwin)
+@osx_only begin
+	using Homebrew
+	provides(Homebrew.HB, "terminal-notifier", terminalnotifier, os = :Darwin)
+end
+
+@BinDeps.install @compat Dict(:terminalnotifier => :terminalnotifier)
--- src/OSXNotifier.jl
+++ src/OSXNotifier.jl
@@ -1,5 +1,11 @@
 module OSXNotifier
 
+if isfile(joinpath(dirname(@__FILE__),"..","deps","deps.jl"))
+    include("../deps/deps.jl")
+else
+    error("OSXNotifier not properly installed. Please run Pkg.build(\"OSXNotifier\")")
+end
+
 export notify
 
 function notify(message)
